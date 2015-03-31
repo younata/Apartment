783892ba8bd32df1ffee31cfc2e1b8bdcbeb225d
Rachel Brindle <you@subluminal.net>
2015-03-30 21:44:25 -0700

    Initial Commit

---
+++ Apartment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Apartment
+//
+//  Created by Rachel Brindle on 3/30/15.
+//  Copyright (c) 2015 Rachel Brindle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
