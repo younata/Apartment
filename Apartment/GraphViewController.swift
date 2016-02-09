765bc380e736d1f5a0b108de0079eaf1e04f5c43
Rachel Brindle <rbrindle@pivotal.io>
2016-02-09 15:03:02 -0800

    Take a pretty stick to the GraphViewController

--- Apartment/GraphViewController.swift
+++ Apartment/GraphViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import ApartKit
+import PureLayout
 import SwiftCharts
 
 public class GraphViewController: UIViewController {
@@ -15,10 +16,35 @@ public class GraphViewController: UIViewController {
         return self.injector!.create(HomeRepository)!
     }()
 
+    private let scrollView = UIScrollView(forAutoLayout: ())
+
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(self.scrollView)
+
+        self.scrollView.autoPinEdgesToSuperviewEdges()
+        self.scrollView.contentOffset = CGPoint.zero
+        self.scrollView.scrollEnabled = true
+        self.scrollView.maximumZoomScale = 4
+        self.scrollView.minimumZoomScale = 1
+        self.scrollView.delegate = self
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.changeScrollViewSize(self.view.bounds.size)
+    }
+
+    public override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+        if let chart = self.chart {
+            self.changeScrollViewSize(size)
+            self.scrollView.contentOffset = CGPoint.zero
+            chart.view.bounds = CGRect(x: 0, y: 0, width: self.scrollView.contentSize.width, height: self.scrollView.contentSize.height)
+            chart.clearView()
+        }
     }
 
     public private(set) var chart: Chart? {
@@ -27,9 +53,18 @@ public class GraphViewController: UIViewController {
                 old.view.removeFromSuperview()
             }
             if let new = chart {
-                self.view.addSubview(new.view)
+                new.view.clipsToBounds = false
+                self.scrollView.addSubview(new.view)
+                self.scrollView.setZoomScale(1, animated: true)
             }
         }
+    }
+
+    private func changeScrollViewSize(size: CGSize) {
+        let height = self.scrollView.contentInset.top + self.scrollView.contentInset.bottom
+
+        self.scrollView.contentSize = CGSizeMake(size.width, size.height - height)
+        self.scrollView.setZoomScale(1, animated: true)
     }
 
     private func retrieveHistory() {
@@ -50,7 +85,7 @@ public class GraphViewController: UIViewController {
                     }
                     let isNumericalData = states.filter({ $0.sensorState == nil }).isEmpty
 
-                    let frame = self.view.bounds.insetBy(dx: 10, dy: 40).offsetBy(dx: 0, dy: 40)
+                    let frame = CGRect(x: 0, y: 0, width: self.scrollView.contentSize.width, height: self.scrollView.contentSize.height)
 
                     if isNumericalData {
                         self.chart = self.plotLineGraph(states, frame: frame)
@@ -61,6 +96,15 @@ public class GraphViewController: UIViewController {
             }
         }
     }
+
+    private let chartSettings: ChartSettings = {
+        let chartSettings = ChartSettings()
+        chartSettings.top = 10
+        chartSettings.trailing = 20
+        chartSettings.leading = 10
+        chartSettings.bottom = 10
+        return chartSettings
+    }()
 
     private func plotLineGraph(states: [State], frame: CGRect) -> LineChart {
         let initialDate: Double
@@ -88,17 +132,21 @@ public class GraphViewController: UIViewController {
             }
         }
 
-        let axisData: (min: Double, max: Double) = pointData.reduce((min: Double.infinity, max: -Double.infinity)) {
-            let lowest = min($0.min, $1.1)
-            let highest = max($0.max, $1.1)
+        let axisData: (min: Double, max: Double) = pointData.reduce((min: Double.infinity, max: -Double.infinity)) { extreme, point in
+            let lowest = min(extreme.min, point.1)
+            let highest = max(extreme.max, point.1)
             return (min: lowest, max: highest)
         }
 
-        let step = max((axisData.max - axisData.min) / 10.0, 0.1)
+        let range = axisData.max - axisData.min
+        let chartMax = ceil(axisData.max + (range / 10))
+        let chartMin = floor(axisData.min - (range / 20))
+        let step = max((chartMax - chartMin) / 10.0, 0.1)
 
         let chartConfig = ChartConfigXY(
-            xAxisConfig: ChartAxisConfig(from: 0, to: mostRecentDate - initialDate, by: 1),
-            yAxisConfig: ChartAxisConfig(from: axisData.min - (step / 2), to: axisData.max + (step / 2), by: step)
+            chartSettings: self.chartSettings,
+            xAxisConfig: ChartAxisConfig(from: 0, to: (mostRecentDate - initialDate) + 1, by: 1),
+            yAxisConfig: ChartAxisConfig(from: chartMin, to: chartMax, by: step)
         )
 
         return LineChart(frame: frame,
@@ -124,12 +172,13 @@ public class GraphViewController: UIViewController {
         }
 
         let barData = data.map { ($0.desnake, $1 / 3600) }
-        let axisMax = barData.reduce(Double(0)) {
-            return max($0, $1.1)
-        }
+        let axisMax = ceil(barData.reduce(Double(0), combine: {  max($0, $1.1) }))
         let step = max(axisMax / 10.0, 0.1)
 
-        let chartConfig = BarsChartConfig(valsAxisConfig: ChartAxisConfig(from: 0, to: axisMax + step, by: step))
+        let chartConfig = BarsChartConfig(
+            chartSettings: self.chartSettings,
+            valsAxisConfig: ChartAxisConfig(from: 0, to: axisMax + step, by: step)
+        )
         return BarsChart(frame: frame,
             chartConfig: chartConfig,
             xTitle: self.entity?.displayName ?? "States",
@@ -152,5 +201,11 @@ public class GraphViewController: UIViewController {
         })
 
         self.presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
+extension GraphViewController: UIScrollViewDelegate {
+    public func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.chart?.view
     }
 }
