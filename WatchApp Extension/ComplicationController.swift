2fefb5d7bc3044b889924970803ae98757907eab
Rachel Brindle <rbrindle@pivotal.io>
2016-02-09 16:02:51 -0800

    Kinda-sorta redo how the complication stuff works

--- WatchApp Extension/ComplicationController.swift
+++ WatchApp Extension/ComplicationController.swift
@@ -3,6 +3,13 @@ import ApartWatchKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource, HomeRepositorySubscriber {
     var homeRepository: HomeRepository?
+    private var entity: State? {
+        didSet {
+            if oldValue != self.entity {
+                self.askComplicationServerToReload()
+            }
+        }
+    }
 
     override init() {
         super.init()
@@ -12,6 +19,20 @@ class ComplicationController: NSObject, CLKComplicationDataSource, HomeRepositor
     }
 
     func didChangeLoginStatus(loggedIn: Bool) {
+        if !loggedIn {
+            self.entity = nil
+        } else {
+            self.reloadComplicationEntity()
+        }
+    }
+
+    func reloadComplicationEntity() {
+        self.homeRepository?.watchComplicationEntity {
+            entity = $0
+        }
+    }
+
+    func askComplicationServerToReload() {
         let complicationServer = CLKComplicationServer.sharedInstance()
         for complication in complicationServer.activeComplications {
             complicationServer.reloadTimelineForComplication(complication)
@@ -37,45 +58,44 @@ class ComplicationController: NSObject, CLKComplicationDataSource, HomeRepositor
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntryForComplication(complication: CLKComplication, withHandler handler: ((CLKComplicationTimelineEntry?) -> Void)) {
-        guard let homeRepository = self.homeRepository else {
+        guard let entity = self.entity else {
             handler(nil)
             return
         }
-        homeRepository.watchComplicationEntity { entity in
-            let longText: String = entity?.displayName ?? "N/A"
-            let shortText: String = entity?.state.desnake ?? "N/A"
 
-            let template : CLKComplicationTemplate?
+        let longText: String = entity.displayName
+        let shortText: String = entity.state.desnake
 
-            switch (complication.family) {
-            case .ModularSmall:
-                let textTemplate = CLKComplicationTemplateModularSmallSimpleText()
-                textTemplate.textProvider = CLKSimpleTextProvider(text: longText, shortText: shortText)
-                template = textTemplate
-            case .ModularLarge:
-                let textTemplate = CLKComplicationTemplateModularLargeStandardBody()
-                textTemplate.headerTextProvider = CLKSimpleTextProvider(text: longText)
-                textTemplate.body1TextProvider = CLKSimpleTextProvider(text: shortText)
-                template = textTemplate
-            case .UtilitarianSmall:
-                let textTemplate = CLKComplicationTemplateUtilitarianSmallRingText()
-                textTemplate.textProvider = CLKSimpleTextProvider(text: shortText)
-                template = textTemplate
-            case .CircularSmall:
-                let textTemplate = CLKComplicationTemplateCircularSmallSimpleText()
-                textTemplate.textProvider = CLKSimpleTextProvider(text: longText, shortText: shortText)
-                template = textTemplate
-            default:
-                template = nil
-            }
-            if let template = template {
-                handler(CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: template))
-            } else {
-                handler(nil)
-            }
+        let template : CLKComplicationTemplate?
+
+        switch (complication.family) {
+        case .ModularSmall:
+            let textTemplate = CLKComplicationTemplateModularSmallSimpleText()
+            textTemplate.textProvider = CLKSimpleTextProvider(text: longText, shortText: shortText)
+            template = textTemplate
+        case .ModularLarge:
+            let textTemplate = CLKComplicationTemplateModularLargeStandardBody()
+            textTemplate.headerTextProvider = CLKSimpleTextProvider(text: longText)
+            textTemplate.body1TextProvider = CLKSimpleTextProvider(text: shortText)
+            template = textTemplate
+        case .UtilitarianSmall:
+            let textTemplate = CLKComplicationTemplateUtilitarianSmallRingText()
+            textTemplate.textProvider = CLKSimpleTextProvider(text: shortText)
+            template = textTemplate
+        case .CircularSmall:
+            let textTemplate = CLKComplicationTemplateCircularSmallSimpleText()
+            textTemplate.textProvider = CLKSimpleTextProvider(text: longText, shortText: shortText)
+            template = textTemplate
+        default:
+            template = nil
+        }
+        if let template = template {
+            handler(CLKComplicationTimelineEntry(date: NSDate(), complicationTemplate: template))
+        } else {
+            handler(nil)
         }
     }
-    
+
     func getTimelineEntriesForComplication(complication: CLKComplication, beforeDate date: NSDate, limit: Int, withHandler handler: (([CLKComplicationTimelineEntry]?) -> Void)) {
         handler(nil)
     }
@@ -91,6 +111,10 @@ class ComplicationController: NSObject, CLKComplicationDataSource, HomeRepositor
     // MARK: - Update Scheduling
     
     func getNextRequestedUpdateDateWithHandler(handler: (NSDate?) -> Void) {
-        handler(NSDate(timeIntervalSinceNow: 300));
+        handler(NSDate(timeIntervalSinceNow: 360))
+    }
+
+    func requestedUpdateDidBegin() {
+        self.reloadComplicationEntity()
     }
 }
